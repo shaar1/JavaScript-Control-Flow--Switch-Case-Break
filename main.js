a8c7f1e866da0c58ac25bcf847db4ae9339871ac
Abdallah <abdallahchaar90@gmail.comh>
2021-01-08 12:43:14 +0100

    träning om flera case

--- main.js
+++ main.js
@@ -28,9 +28,28 @@ var season = prompt("What The Best Season for You?");
 
 switch (season) {
 
-    case "winter":
+    case "Winter":
 
         alert("Winter Is Cold");
 
         break;
+    case "Summer":
+
+        alert("Summer Is Too Hot");
+
+        break;
+
+    case "Autumn":
+
+        alert("Autumn Is Very Good");
+
+        break;
+
+    case "Spring":
+
+        alert("Spring Is Amazing");
+
+        break;
+
+
 }
