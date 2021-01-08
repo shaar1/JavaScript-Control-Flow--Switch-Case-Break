5e740f3ac45286e69905fe7016e88fea8dc98cd2
Abdallah <abdallahchaar90@gmail.comh>
2021-01-08 12:38:42 +0100

    träning om JavScript med Switch och Case

---
+++ main.js
@@ -0,0 +1,36 @@
+/*  global console, alert, prompt */
+
+/*
+switch (expression)
+
+case x:
+    Code To Exceute;
+    break;
+
+    case x:
+    Code To Exceute;
+    break;
+
+    case x:
+    Code To Exceute;
+    break;
+
+    case x:
+    Code To Exceute;
+    break;
+
+   default:
+    Code To Exceute;
+    break;
+*/
+
+var season = prompt("What The Best Season for You?");
+
+switch (season) {
+
+    case "winter":
+
+        alert("Winter Is Cold");
+
+        break;
+}
